93a2cbd355e6c217b8d588d80615bb948a4184a5
Oleh Hvozdetskyi <oleg.gvozdetskyi@gmail.com>
2025-05-29 01:38:56 +0300

    Add FXs

--- Assets/CodeBase/Scripts/Characters/Vehicles/Vehicle.cs
+++ Assets/CodeBase/Scripts/Characters/Vehicles/Vehicle.cs
@@ -4,21 +4,34 @@ using CodeBase.Scripts.Service;
 using CodeBase.Scripts.Weapons;
 using Unavinar.Pooling;
 using UnityEngine;
+using Zenject;
 using static CodeBase.Scripts.Utils.Enums;
 
 namespace CodeBase.Scripts.Characters.Vehicles
 {
     public class Vehicle : PoolableMonoBehaviour
     {
+        [SerializeField] private PoolableParticle dieFxPrefab;
         [SerializeField] private VehicleMovement vehicleMovement;
         [SerializeField] private WeaponRotator weaponRotator;
         [SerializeField] private TouchController touchController;
         [field: SerializeField] public VehicleDamageableObject DamageableObject { get; private set; }
 
+        private ObjectPool _objectPool;
+
+        [Inject]
+        private void Construct(ObjectPool objectPool)
+        {
+            _objectPool = objectPool;
+        }
+
         private void OnEnable()
         {
             Subscribe();
-            OnDiedHandler();
+
+            vehicleMovement.enabled = false;
+            weaponRotator.enabled = false;
+            touchController.enabled = false;
         }
 
         private void Subscribe()
@@ -47,6 +60,16 @@ namespace CodeBase.Scripts.Characters.Vehicles
             vehicleMovement.enabled = false;
             weaponRotator.enabled = false;
             touchController.enabled = false;
+
+            SpawnFx();
+        }
+
+        private void SpawnFx()
+        {
+            if (dieFxPrefab == null) return;
+
+            var dieFx = _objectPool.Get(dieFxPrefab);
+            dieFx.transform.position = transform.position;
         }
 
         private void OnDisable()
